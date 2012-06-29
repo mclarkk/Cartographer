4cd063a41b9c9f653d8e104cb8a3155804a55baf
Meghan Clark <mclark@oakwoodcontrols.com>
2012-06-29 18:27:49 -0400

    starting pseudocode

---
+++ src/main.py
@@ -0,0 +1,31 @@
+# Author: Meghan Clark
+# Date: 6/29/12
+# Description: The main method for Cartographer.
+
+# Get batch parameters
+# These should come from a file at some point.
+dimension = 2
+point_counts = [10, 100]
+core_counts = [5, 10]
+mappings = ['hilbert', 'zorder']
+nearest_neighbor_counts = [2, 3]
+nearness_ranges = [1, 2]
+
+# Run experiment batch.
+
+# sort point & core counts to be used as stacks.
+
+# while point count not empty
+# pop point count
+# generate point_count points
+# construct distance matrix
+# get ordered list of near neighbors (within distances r0-ri)
+# get ordered list of nearest neighbors (for k0-ki # of neighbors)
+# metric: get avg. distance to k0-ki nearest neighbors,
+
+
+
+#
+
+
+# Dump data to file.
